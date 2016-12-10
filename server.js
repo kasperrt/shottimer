807b4de23bf9e896bc2d29bc6820d80146f5f604
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-12-10 16:23:10 +0100

    Re-added required functions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var crypto = require('crypto');
+var unique_ids = [];
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -10,7 +11,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   var socketid = socket.id.substring(2);
   var guid = uniqueID(socketid, 4);
-
+  unique_ids.push(guid);
   socket.on("join", function(obj){
     io.to(obj.id).emit("joined", {_name: obj.name});
   });
@@ -27,6 +28,23 @@ function hash_pass(thing)
     return crypto.createHash('sha256').update(thing).digest('base64');
 }
 
+function rndName(seed, len) {
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    consts =  ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'w', 'x', 'y'];
+    //len = 8;
+    len = Math.floor(len);
+    word = '';
+    is_vowel = false;
+    var arr;
+    for (var i = 0; i < len; i++) {
+        if (is_vowel) arr = vowels;
+        else arr = consts;
+        is_vowel = !is_vowel;
+        word += arr[(seed[i%seed.length].charCodeAt()+i) % (arr.length-1)];
+    }
+    return word;
+}
+
 function uniqueID(seed, minlen){
     var len = minlen;
     var id = rndName(seed, minlen);
@@ -36,6 +54,20 @@ function uniqueID(seed, minlen){
         len += 0.1;                        // try 10 times at each length
     }
     return id;
+}
+
+function contains(a, obj) {
+    try{
+        var i = a.length;
+        while (i--) {
+            if (a[i] === obj) {
+                return true;
+            }
+        }
+        return false;
+    }catch(e){
+        return false;
+    }
 }
 
 http.listen(3000, function(){
