b6ee449aa679bfcc5cf197f7698e3e4597e32add
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-12-10 16:26:07 +0100

    qr-code image is no longer a link

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ socket.on("joined", function(obj){
 
 socket.on("id", function(_id){
 	id = encodeURI("?" + _id);
-	document.getElementById("qr_container").innerHTML = "<a href='http://etys.no/mobile.html" + id + "'>'<img src='//chart.googleapis.com/chart?chs=150x150&cht=qr&chl=http://etys.no/mobile.html" + id + "&choe=UTF-8&chld=L%7C1' alt='qr' /></a>";
+	document.getElementById("qr_container").innerHTML = "<img src='//chart.googleapis.com/chart?chs=150x150&cht=qr&chl=http://etys.no/mobile.html" + id + "&choe=UTF-8&chld=L%7C1' alt='qr' />";
 });
 socket.emit("host");
 
