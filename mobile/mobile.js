ea7c6d0416cf39fe3bab8fe00ea28809afbfbf93
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-12-11 18:58:52 +0100

    Updated message hwne joined game

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -4,7 +4,9 @@ window.addEventListener("load", function(){
   document.getElementById("playerform").addEventListener("submit", function(e){
 		e.preventDefault();
 
-		socket.emit("join", {id: window.location.search.substring(1), name: this.name.value})
-		window.location = "https://zoff.no";
-	});	
+		socket.emit("join", {id: window.location.search.substring(1), name: this.name.value});
+    document.getElementsByClassName("valign")[0].innerHTML = "<div style='text-align:center;font-size:4rem;'>You have joined the game!</span>";
+    socket.disconnect();
+		//window.location = "https://zoff.no";
+	});
 });
