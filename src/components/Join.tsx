1cf20803b770afba179157b6086425107f2165ea
Kasper Rynning-Tønnesen <github@bundle.email>
2025-01-21 22:53:05 +0100

    fix: Some cleaner use

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -5,22 +5,28 @@ import { Show, createEffect, createSignal } from 'solid-js';
 export function Join() {
   const { gameId } = game;
   const [code, setCode] = createSignal<string>('');
+  const joinUrl = () => `${window.location.protocol}//${window.location.host}/${gameId()}`;
 
   createEffect(async () => {
-    const c = await qrcode.toDataURL(`${window.location.protocol}//${window.location.host}/${gameId()}`);
+    const c = await qrcode.toDataURL(joinUrl());
     setCode(c);
   });
 
   return (
-    <Show when={gameId()}>
-      <div class="flex w-full flex-col items-center justify-center">
-        <div class="text-center">Join {`${window.location.protocol}//${window.location.host}/${gameId()}`}</div>
-        <img
-          src={code()}
-          alt={`Join ${window.location.protocol}//${window.location.host}/${gameId()}`}
-          class="size-28"
-        />
-      </div>
-    </Show>
+    <div class="h-40">
+      <Show when={gameId()}>
+        <div class="flex w-full flex-col items-center justify-center">
+          <div class="text-center">
+            Join{' '}
+            <a href={joinUrl()} target="_blank" rel="noreferrer">
+              {joinUrl()}
+            </a>
+          </div>
+          <div class="size-28">
+            <img src={code()} alt={`Join ${joinUrl()}`} class="size-28" />
+          </div>
+        </div>
+      </Show>
+    </div>
   );
 }
