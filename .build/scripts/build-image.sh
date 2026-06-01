394e8e81cc0745044b279d9f1377ed64872273b4
Kasper Rynning-Tønnesen <github@bundle.email>
2026-06-01 19:55:05 +0200

    Move ShotTimer to shot.etys.no (#9)

--- .build/scripts/build-image.sh
+++ .build/scripts/build-image.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 IMAGE="ghcr.io/${GITHUB_REPOSITORY,,}:${GITHUB_SHA}"
 DOCKERFILE="${DOCKERFILE:-Dockerfile}"
 CONTEXT="${CONTEXT:-.}"
-VITE_HOST="${VITE_HOST:-https://etys.no}"
-VITE_CORS="${VITE_CORS:-https://etys.no,https://www.etys.no}"
+VITE_HOST="${VITE_HOST:-https://shot.etys.no}"
+VITE_CORS="${VITE_CORS:-https://shot.etys.no}"
 docker build \
   --build-arg "VITE_HOST=${VITE_HOST}" \
   --build-arg "VITE_CORS=${VITE_CORS}" \
